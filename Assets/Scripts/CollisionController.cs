d9d9497b95ed0768055cdac8658980db92fd74eb
valje <valli_t@live.de>
2020-04-01 15:42:22 +0200

    generic collision detection

---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+
+    public Transform objectCenter;
+    public float checkDistance = 0.4f;
+    public LayerMask collisionMask;
+
+
+    bool isCollision;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCollision = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isCollision = Physics.CheckSphere(objectCenter.position, checkDistance, collisionMask);
+        if (isCollision) { 
+            print("collision");
+        }
+        isCollision = false;
+    }
+}
