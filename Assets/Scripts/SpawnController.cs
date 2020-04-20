1c8c7d01dda5e0c2e8a2c5f09a6bf6b6c400415c
TobiKoetter <tobias-koetter@gmx.de>
2020-04-20 14:00:16 +0200

    SpawnController renaming issue: Try to resolve problem

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -18,7 +18,7 @@ public class SpawnController : MonoBehaviour
     void Start()
     {
 
-        if(maxRadius < minRadius)
+        if (maxRadius < minRadius)
         {
             throw new System.Exception("SpawnController: maxRadius is smaller then the planed min Distance to the player");
         }
@@ -29,7 +29,7 @@ public class SpawnController : MonoBehaviour
         // serves as a unique number for every trash element
         int counter = 0;
 
-        foreach(Transform spP in spawnPool)
+        foreach (Transform spP in spawnPool)
         {
             for (int i = 0; i < spP.childCount; i++)
             {
@@ -40,14 +40,14 @@ public class SpawnController : MonoBehaviour
                 curObject.gameObject.SetActive(false);
             }
         }
-        
+
         spawned = new List<Spawned>();
     }
 
     void Update()
     {
 
-        if(Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             spawnOnTimer();
         }
@@ -72,20 +72,20 @@ public class SpawnController : MonoBehaviour
     private void spawnObject()
     {
 
-    // Spawn Object in a Circle around the set Spawnpoint with fixed y-Axis Value
-    // Center = spawnPoint
-    // Radius = between minRadius and maxRadius (min/max Distance to the player)
+        // Spawn Object in a Circle around the set Spawnpoint with fixed y-Axis Value
+        // Center = spawnPoint
+        // Radius = between minRadius and maxRadius (min/max Distance to the player)
 
 
-    // Find Random radius size between possible min and max
+        // Find Random radius size between possible min and max
         float curRadius = Random.Range(minRadius, maxRadius);
 
-/* -[Create Position behind Player]
- *   Find random vector on a unit Circle border and put it in a Vector3
- *   Random.insideUnitCircle returns Point inside a unit Circle and when normalized its placed on the borderline of the unit circle
- *   is facing (negative forward vector + created random direction)
- *   The calculated spawn should be behind the player, so he doesn't see the spawn.
- */
+        /* -[Create Position behind Player]
+         *   Find random vector on a unit Circle border and put it in a Vector3
+         *   Random.insideUnitCircle returns Point inside a unit Circle and when normalized its placed on the borderline of the unit circle
+         *   is facing (negative forward vector + created random direction)
+         *   The calculated spawn should be behind the player, so he doesn't see the spawn.
+         */
         Vector2 posXZ = Random.insideUnitCircle.normalized;
         Vector3 randDir = new Vector3(posXZ.x, 0f, posXZ.y);
         Vector3 tempPoint = center.position + ((-center.forward + randDir).normalized * curRadius);
@@ -93,8 +93,8 @@ public class SpawnController : MonoBehaviour
         Quaternion tempRotation = Random.rotation;
 
 
-    // Object gets picked out of the SpawnPool and its components get reset for scene interaction
-        Spawned current = spawnable[Random.Range(0,spawnable.Count)];
+        // Object gets picked out of the SpawnPool and its components get reset for scene interaction
+        Spawned current = spawnable[Random.Range(0, spawnable.Count)];
         current.gameObject.transform.position = tempPoint;
         current.gameObject.transform.rotation = tempRotation;
         current.gameObject.SetActive(true);
@@ -106,7 +106,7 @@ public class SpawnController : MonoBehaviour
     public void despawnObjectWithID(int PoolNumber)
     {
         Spawned current = null;
-        foreach(Spawned i in spawned)
+        foreach (Spawned i in spawned)
         {
             if (i.PoolNumber == PoolNumber)
             {
