f717447511ad3b3e980ebb784663701068346ed5
TobiKoetter <tobias-koetter@gmx.de>
2020-05-19 16:11:13 +0200

    Added Pickup Class and testing pickup small trash (not in blockout scene yet) needs more fine tuning

--- Assets/Scripts/PickupManager.cs
+++ Assets/Scripts/PickupManager.cs
@@ -13,6 +13,10 @@ public class PickupManager : MonoBehaviour
 
     private Material[] oldMat;
     private Spawned lastPickup;
+    private Quaternion inHandRotation = Quaternion.Euler(-65f,150f,90f);
+    private Quaternion originalRotation;
+
+    private Vector3 originalScale;
 
     void Start()
     {
@@ -33,6 +37,7 @@ public class PickupManager : MonoBehaviour
                     lastPickup = hitInfo.collider.gameObject.GetComponent<Spawned>();
                     Debug.Log("Pickup Triggered with " + lastPickup);
                     //Debug.Log(lastPickup);
+                    /*
                     MeshRenderer mR = lastPickup.getRenderer();
                     Material[] mats = mR.materials;
                     oldMat = new Material[mats.Length];
@@ -42,16 +47,21 @@ public class PickupManager : MonoBehaviour
                         mats[i] = highlight;
                     }
                     mR.materials = mats;
-
-                    lastPickup.transform.parent = hand;
-                    lastPickup.transform.localPosition = Vector3.zero;
+                    */
+                    originalScale = lastPickup.transform.localScale;
                     lastPickup.getBody().isKinematic = true;
+                    lastPickup.transform.SetParent(hand, false);
+                    lastPickup.transform.SetPositionAndRotation(hand.position + hand.up*0.15f + hand.forward*0.1f, hand.rotation*inHandRotation);
+                    Vector3 scale = lastPickup.transform.localScale;
+                    lastPickup.transform.localScale = new Vector3(scale.x/hand.localScale.x, scale.y/hand.localScale.y, scale.z/hand.localScale.z);
+                    
 
                 }
             }
             else if (lastPickup != null && !hit)
             {
                 Debug.Log("Do we get here?");
+                /*
                 MeshRenderer mR = lastPickup.getRenderer();
                 Material[] mats = mR.materials;
                 for (int i = 0; i < mats.Length; i++)
@@ -59,10 +69,19 @@ public class PickupManager : MonoBehaviour
                     mats[i] = oldMat[i];
                 }
                 mR.materials = mats;
+                */
                 lastPickup.transform.parent = null;
                 lastPickup.getBody().isKinematic = false;
+                lastPickup.transform.localScale = originalScale;
                 lastPickup = null;
             }
         }
+/*
+        if(lastPickup != null)
+        {
+            lastPickup.transform.position = hand.position + (hand.up * 0.1f + hand.forward*0.2f);
+            lastPickup.transform.rotation = hand.rotation* inHandRotation;
+        }
+        */
     }
 }
